3939afbfc5088e4b7715d8838fc0c578e2e03e97
Bruno Pessoa <brunofernpessoa@gmail.com>
2022-07-05 13:31:04 -0300

    Adiciona requisito 13

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,12 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header/Header';
+import { getUser } from '../../services/userAPI';
+import Loading from '../../components/Loading';
+import styles from './Profile.module.css';
 
-function Profile() {
-  return (
-    <div data-testid="page-profile">
-      <Header />
-    </div>
-  );
+class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {},
+      loading: true,
+    };
+  }
+
+  componentDidMount = async () => {
+    const user = await getUser();
+    this.setState({ user, loading: false });
+  };
+
+  render() {
+    const { user: { name, email, image, description }, loading } = this.state;
+    return (
+      <div data-testid="page-profile" className={ styles.main }>
+        <Header />
+        {loading ? <Loading /> : (
+          <main className={ styles.profile }>
+            <div className={ styles.edit }>
+              <img src={ image } alt={ name } data-testid="profile-image" />
+              <Link to="/profile/edit">Editar perfil</Link>
+            </div>
+            <span>Nome:</span>
+            <p>{name}</p>
+            <span>E-mail:</span>
+            <p>{email}</p>
+            <span>Descrição:</span>
+            <p>{description}</p>
+          </main>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Profile;
